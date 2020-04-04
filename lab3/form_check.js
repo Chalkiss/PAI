76838b40bf8287ca4efc897d0d40692986935b20
FGap <filgap@wp.pl>
2020-04-04 16:05:37 +0200

    "Final" version

--- lab3/form_check.js
+++ lab3/form_check.js
@@ -8,20 +8,6 @@ checkStringAndFocus(form.elements["f_ulica"], "Puste pole ulica!");
 checkStringAndFocus(form.elements["f_miasto"], "Puste pole miasto!");
 }
 
-function isWhiteSpaceOrEmpty(str){
-    return /^[\s\t\r\n]*$/.test(str);
-}
-
-function checkString(s, message){
-    if (isWhiteSpaceOrEmpty(s)== true){
-        alert(message);
-        return false;
-    }
-    else{
-        return true;
-    }
-}
-
 function checkEmailAndFocus(obj, msg){
     let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
     let str = obj.value;
@@ -46,4 +32,63 @@ function checkStringAndFocus(obj, msg){
     else{
         return true;
     }
+}
+
+function isWhiteSpaceOrEmpty(str){
+    return /^[\s\t\r\n]*$/.test(str);
+}
+
+function showElement(e,z){
+    document.getElementById(e).style.visibility = 'visible';
+    document.getElementById(z).style.visibility = 'visible';
+    
+}
+
+function hideElement(e,z){
+    document.getElementById(e).style.visibility = 'hidden';
+    document.getElementById(z).style.visibility = 'hidden';
+
+}
+
+function alterRows(i,e){
+    if (e) {
+        if (i % 2 == 1) {
+            e.setAttribute("style", "background-color: Aqua;");
+        }
+        e = e.nextSibling;
+        while( e && e.nodeType!= 1){
+            e = e.nextSibling;
+        }
+        alterRows(++i, e);
+    }
+}
+
+function nextNode(e){
+    while(e && e.nodeType != 1){
+        e = e.nextSibling;
+    }
+    return e;
+}
+
+function prevNode(e){
+    while(e && e.nodeType != 1){
+        e = e.previousSibling;
+    }
+    return e;
+}
+
+function swapRows(b){
+    let tab = prevNode(b.previousSibling);
+    let tBody = nextNode(tab.firstChild);
+    let lastNode = prevNode(tBody.lastChild);
+    tBody.removeChild(lastNode);
+    let firstNode = nextNode(tBody.firstChild);
+    tBody.insertBefore(lastNode, firstNode);
+}
+
+function cnt(form, msg, maxSize){
+    if(form.value.length > maxSize)
+        form.value = form.value.substring(0,maxSize);
+    else
+        msg.innerHTML = maxSize - form.value.length;
 }
